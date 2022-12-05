beb0e9037e776245ca0270dddde818891e82b024
Presiyana Borisova <presiana03@gmail.com>
2022-12-05 14:43:56 +0200

    Db Models completed

--- ArtGallery.Infrastructure/Migrations/20221205124219_CompleteModelsCreation.Designer.cs
+++ ArtGallery.Infrastructure/Migrations/20221205124219_CompleteModelsCreation.Designer.cs
@@ -4,6 +4,7 @@ using ArtGallery.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ArtGallery.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221205124219_CompleteModelsCreation")]
+    partial class CompleteModelsCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
